b8923eb78a230d677c0ea4de313e408e4bebcde5
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-03-26 22:17:36 +0800

    add FCOS-RT-OTA

--- config/fcos_config.py
+++ config/fcos_config.py
@@ -54,7 +54,7 @@ fcos_config = {
         'conf_thresh': 0.05,
         'nms_thresh': 0.6,
         # anchor box
-        'regression_range': [[-1, 64], [64, 128], [128, 256], [256, 512], [512, float('inf')]],
+        'object_sizes_of_interest': [[-1, 64], [64, 128], [128, 256], [256, 512], [512, float('inf')]],
         # matcher
         'matcher': 'matcher',
         'ctr_clamp': 32,
@@ -137,7 +137,7 @@ fcos_config = {
         'conf_thresh': 0.05,
         'nms_thresh': 0.6,
         # anchor box
-        'regression_range': [[-1, 64], [64, 128], [128, 256], [256, 512], [512, float('inf')]],
+        'object_sizes_of_interest': [[-1, 64], [64, 128], [128, 256], [256, 512], [512, float('inf')]],
         # matcher
         'matcher': 'matcher',
         'ctr_clamp': 32,
@@ -220,7 +220,7 @@ fcos_config = {
         'conf_thresh': 0.05,
         'nms_thresh': 0.6,
         # anchor box
-        'regression_range': [[-1, 64], [64, 128], [128, 256], [256, 512], [512, float('inf')]],
+        'object_sizes_of_interest': [[-1, 64], [64, 128], [128, 256], [256, 512], [512, float('inf')]],
         # matcher
         'matcher': 'matcher',
         'ctr_clamp': 32,
@@ -303,11 +303,94 @@ fcos_config = {
         'conf_thresh': 0.05,
         'nms_thresh': 0.6,
         # anchor box
-        'regression_range': [[-1, 64], [64, 128], [128, float('inf')]],
+        'object_sizes_of_interest': [[-1, 64], [64, 128], [128, float('inf')]],
         # matcher
         'matcher': 'matcher',
         'ctr_clamp': 32,
         'center_sampling_radius': 1.5,
+        # loss
+        'alpha': 0.25,
+        'gamma': 2.0,
+        'loss_cls_weight': 1.0,
+        'loss_reg_weight': 1.0,
+        'loss_ctn_weight': 1.0,
+        # optimizer
+        'optimizer': 'sgd',
+        'momentum': 0.9,
+        'weight_decay': 1e-4,
+        'warmup': 'linear',
+        'wp_iter': 1500,
+        'warmup_factor': 0.00066667,
+        'epoch': {
+            '1x': {'max_epoch': 12, 
+                    'lr_epoch': [8, 11], 
+                    'multi_scale': None},
+            '2x': {'max_epoch': 24, 
+                    'lr_epoch': [16, 22], 
+                    'multi_scale': [448, 480, 512, 544, 576, 608, 640]},
+            '3x': {'max_epoch': 36, 
+                    'lr_epoch': [24, 33], 
+                    'multi_scale': [448, 480, 512, 544, 576, 608, 640]},
+        },
+    },
+
+    'fcos-rt-ota': { # Real Time FCOS with OTA
+        # input
+        'min_size': 640,
+        'max_size': 640,
+        'format': 'RGB',
+        'pixel_mean': [0.485, 0.456, 0.406],
+        'pixel_std': [0.229, 0.224, 0.225],
+        'transforms': {
+            '1x':[{'name': 'RandomHorizontalFlip'},
+                  {'name': 'RandomShift', 'max_shift': 32},
+                  {'name': 'ToTensor'},
+                  {'name': 'Resize'},
+                  {'name': 'Normalize'},
+                  {'name': 'PadImage'}],
+
+            '2x':[{'name': 'RandomHorizontalFlip'},
+                  {'name': 'RandomShift', 'max_shift': 32},
+                  {'name': 'ToTensor'},
+                  {'name': 'Resize'},
+                  {'name': 'Normalize'},
+                  {'name': 'PadImage'}],
+
+            '3x':[{'name': 'DistortTransform',
+                   'hue': 0.1,
+                   'saturation': 1.5,
+                   'exposure': 1.5},
+                  {'name': 'RandomHorizontalFlip'},
+                  {'name': 'RandomShift', 'max_shift': 32},
+                  {'name': 'JitterCrop', 'jitter_ratio': 0.3},
+                  {'name': 'ToTensor'},
+                  {'name': 'Resize'},
+                  {'name': 'Normalize'},
+                  {'name': 'PadImage'}]},
+        # model
+        'backbone': 'resnet50',
+        'norm_type': 'FrozeBN',
+        'stride': [8, 16, 32],
+        'act_type': 'relu',
+        # neck
+        'fpn': 'basic_fpn',
+        'from_c5': False,
+        'p6_feat': False,
+        'p7_feat': False,
+        # head
+        'head_dim': 256,
+        'head': 'decoupled_head',
+        'num_cls_head': 4,
+        'num_reg_head': 4,
+        # post process
+        'conf_thresh': 0.05,
+        'nms_thresh': 0.6,
+        # matcher
+        'matcher': 'ota_matcher',
+        'eps': 0.1, 
+        'max_iter': 50,
+        'ctr_clamp': 32,
+        'center_sampling_radius': 2.5,
         # loss
         'alpha': 0.25,
         'gamma': 2.0,
