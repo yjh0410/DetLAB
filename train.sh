d007d8da4c77cd84ec1a81400acddc7ec234ffbc
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-03-26 09:31:02 +0800

    train yolof-rt

--- train.sh
+++ train.sh
@@ -2,9 +2,9 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v yolof50 \
+        -v yolof-rt \
         -lr 0.03 \
         -lr_bk 0.01 \
         --batch_size 16 \
-        --schedule 1x \
+        --schedule 3x \
         --grad_clip_norm 4.0 \
