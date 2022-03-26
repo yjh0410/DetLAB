2d486cca593ecbff3f8f5046845640af92016b4d
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-03-26 21:02:35 +0800

    train retinanet18

--- train.sh
+++ train.sh
@@ -2,9 +2,9 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v yolof-rt \
-        -lr 0.03 \
+        -v retinanet18 \
+        -lr 0.01 \
         -lr_bk 0.01 \
         --batch_size 16 \
-        --schedule 3x \
+        --schedule 1x \
         --grad_clip_norm 4.0 \
