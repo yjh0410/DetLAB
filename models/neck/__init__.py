fb6561314a25698ef89bd1a56f71cc39ed6f6e0b
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-03-26 09:30:46 +0800

    update

--- models/neck/__init__.py
+++ models/neck/__init__.py
@@ -13,6 +13,8 @@ def build_neck(cfg, in_dim, out_dim):
                               expand_ratio=cfg['expand_ratio'], 
                               dilation_list=cfg['dilation_list'],
                               act_type=cfg['act_type'])
+    elif model == 'spp':
+        neck = None
 
     return neck
 
@@ -28,6 +30,15 @@ def build_fpn(cfg, in_dims, out_dim, from_c5=False, p6_feat=False, p7_feat=False
                            from_c5=from_c5, 
                            p6_feat=p6_feat,
                            p7_feat=p7_feat)
+
+    elif model == 'basic_pan':
+        fpn_net = None
+        
+    elif model == 'yolo_fpn':
+        fpn_net = None
+
+    elif model == 'yolo_pafpn':
+        fpn_net = None
 
     return fpn_net
 
