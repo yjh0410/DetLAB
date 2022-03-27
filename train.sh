e3d3286d9c28610ec968523a28a72597d93288e2
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-03-27 21:01:21 +0800

    train retinanet18

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@ python train.py \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
         -v retinanet18 \
-        -lr 0.01 \
-        -lr_bk 0.01 \
-        --batch_size 16 \
+        -lr 0.005 \
+        -lr_bk 0.005 \
+        --batch_size 8 \
         --schedule 1x \
         --grad_clip_norm 4.0 \
