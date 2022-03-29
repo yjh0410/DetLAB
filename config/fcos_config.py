1c3b462797ec50da3e8e12f7862a9789e082b16b
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-03-29 11:00:52 +0800

    update RCOS-OTA config

--- config/fcos_config.py
+++ config/fcos_config.py
@@ -168,6 +168,89 @@ fcos_config = {
         },
     },
 
+    'fcos50': {
+        # input
+        'min_size': 800,
+        'max_size': 1333,
+        'format': 'RGB',
+        'pixel_mean': [0.485, 0.456, 0.406],
+        'pixel_std': [0.229, 0.224, 0.225],
+        'transforms': {
+            '1x':[{'name': 'RandomHorizontalFlip'},
+                  {'name': 'RandomShift', 'max_shift': 32},
+                  {'name': 'ToTensor'},
+                  {'name': 'Resize'},
+                  {'name': 'Normalize'},
+                  {'name': 'PadImage'}],
+
+            '2x':[{'name': 'RandomHorizontalFlip'},
+                  {'name': 'RandomShift', 'max_shift': 32},
+                  {'name': 'ToTensor'},
+                  {'name': 'Resize'},
+                  {'name': 'Normalize'},
+                  {'name': 'PadImage'}],
+
+            '3x':[{'name': 'DistortTransform',
+                   'hue': 0.1,
+                   'saturation': 1.5,
+                   'exposure': 1.5},
+                  {'name': 'RandomHorizontalFlip'},
+                  {'name': 'RandomShift', 'max_shift': 32},
+                  {'name': 'JitterCrop', 'jitter_ratio': 0.3},
+                  {'name': 'ToTensor'},
+                  {'name': 'Resize'},
+                  {'name': 'Normalize'},
+                  {'name': 'PadImage'}]},
+        # model
+        'backbone': 'resnet50',
+        'norm_type': 'FrozeBN',
+        'stride': [8, 16, 32, 64, 128],
+        'act_type': 'relu',
+        # neck
+        'fpn': 'basic_fpn',
+        'from_c5': False,
+        'p6_feat': True,
+        'p7_feat': True,
+        # head
+        'head_dim': 256,
+        'head': 'decoupled_head',
+        'num_cls_head': 4,
+        'num_reg_head': 4,
+        # post process
+        'conf_thresh': 0.05,
+        'nms_thresh': 0.6,
+        # matcher
+        'matcher': 'ota_matcher',
+        'eps': 0.1, 
+        'max_iter': 50,
+        'ctr_clamp': 32,
+        'center_sampling_radius': 2.5,
+        # loss
+        'alpha': 0.25,
+        'gamma': 2.0,
+        'loss_cls_weight': 1.0,
+        'loss_reg_weight': 1.0,
+        'loss_ctn_weight': 1.0,
+        # optimizer
+        'optimizer': 'sgd',
+        'momentum': 0.9,
+        'weight_decay': 1e-4,
+        'warmup': 'linear',
+        'wp_iter': 1500,
+        'warmup_factor': 0.00066667,
+        'epoch': {
+            '1x': {'max_epoch': 12, 
+                    'lr_epoch': [8, 11], 
+                    'multi_scale': None},
+            '2x': {'max_epoch': 24, 
+                    'lr_epoch': [16, 22], 
+                    'multi_scale': [400, 500, 600, 700, 800]},
+            '3x': {'max_epoch': 36, 
+                    'lr_epoch': [24, 33], 
+                    'multi_scale': [400, 500, 600, 700, 800]},
+        },
+    },
+
     'fcos101': {
         # input
         'min_size': 800,
