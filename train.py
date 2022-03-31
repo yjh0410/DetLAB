9c4f31054af58c3bc1d05563a68d5608447b2a67
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-03-31 16:14:46 +0800

    rewrite retinanet

--- train.py
+++ train.py
@@ -186,17 +186,16 @@ def train():
             dataloader.sampler.set_epoch(epoch)            
 
         if distributed_utils.is_main_process():
-            if evaluator is None:
-                print('No evaluator ... save model and go on training.')
-                print('Saving state, epoch: {}'.format(epoch + 1))
-                weight_name = '{}_epoch_{}.pth'.format(args.version, epoch + 1)
-                checkpoint_path = os.path.join(path_to_save, weight_name)
-                torch.save({'model': model_without_ddp.state_dict(),
-                            'optimizer': optimizer.state_dict(),
-                            'lr_scheduler': lr_scheduler.state_dict(),
-                            'epoch': epoch,
-                            'args': args}, 
-                            'eval.pth')                      
+            print('No evaluator ... save model and go on training.')
+            print('Saving state, epoch: {}'.format(epoch + 1))
+            weight_name = '{}_epoch_{}.pth'.format(args.version, epoch + 1)
+            checkpoint_path = os.path.join(path_to_save, weight_name)
+            torch.save({'model': model_without_ddp.state_dict(),
+                        'optimizer': optimizer.state_dict(),
+                        'lr_scheduler': lr_scheduler.state_dict(),
+                        'epoch': epoch,
+                        'args': args}, 
+                        'eval.pth')                      
 
         # train one epoch
         for iter_i, (images, targets, masks) in enumerate(dataloader):
