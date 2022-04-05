776a8e57bb010da19ad96f6f2d5c2020efb5315c
yjh0410 <1394571815@qq.com>
2022-04-05 22:44:15 +0800

    debug retinanet-rt

--- train.sh
+++ train.sh
@@ -2,9 +2,9 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v retinanet-rt \
+        -v yolof-rt \
         -lr 0.01 \
         -lr_bk 0.01 \
         --batch_size 8 \
-        --schedule 4x \
+        --schedule 3x \
         --grad_clip_norm 4.0 \
