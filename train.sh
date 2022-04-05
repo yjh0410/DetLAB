fd6bf81002b7339cc970596efb10ed73221ee299
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-04-05 17:56:51 +0800

    train retinanet with 4x schedule

--- train.sh
+++ train.sh
@@ -2,9 +2,9 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v fcos50 \
+        -v retinanet-rt \
         -lr 0.01 \
         -lr_bk 0.01 \
         --batch_size 16 \
-        --schedule 1x \
+        --schedule 4x \
         --grad_clip_norm 4.0 \
