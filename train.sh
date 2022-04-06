0f7d5a836addc81389232fda8b270bab6c14326b
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-04-06 20:00:46 +0800

    train fcos-rt-ota

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v fcos-rt-ota \
+        -v fcos-rt \
         -lr 0.01 \
         -lr_bk 0.01 \
         --batch_size 16 \
