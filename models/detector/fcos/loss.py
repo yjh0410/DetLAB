a99c483d294d4e9f181011fa63522ca1e8ef4d31
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-04-06 19:58:55 +0800

    train fcos-rt-ota

--- models/detector/fcos/loss.py
+++ models/detector/fcos/loss.py
@@ -159,6 +159,7 @@ class Criterion(object):
             num_bboxes = num_targets
         else: # OTA Matcher
             num_bboxes =  num_foreground
+        print(num_bboxes)
         loss_bboxes = self.loss_bboxes(
             pred_delta[foreground_idxs],
             gt_shifts_deltas[foreground_idxs],
