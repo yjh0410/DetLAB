e41168a08035ec23b6e356f2713fa729d017e138
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-04-15 16:40:19 +0800

    add TTF-YOLO

--- train.sh
+++ train.sh
@@ -2,9 +2,9 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v fcos-rt-ota \
+        -v ttf_yolo19 \
         -lr 0.01 \
         -lr_bk 0.01 \
         --batch_size 16 \
-        --schedule 3x \
+        --schedule 4x \
         --grad_clip_norm 4.0 \
