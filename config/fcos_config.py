9a3527f7c26a26d2983a72c9d3569d3e25c72be2
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-04-19 20:50:15 +0800

    train fcos-rt-ota

--- config/fcos_config.py
+++ config/fcos_config.py
@@ -470,7 +470,7 @@ fcos_config = {
         'norm_type': 'FrozeBN',
         'stride': [8, 16, 32],
         # neck
-        'neck': None,
+        'neck': 'spp',
         'expand_ratio': 1.0,
         'kernel_sizes': [5, 9, 13],
         'neck_norm': 'GN',
