caa625d6fd2e8e3f1e5b81c47b4f53eaf314d3c0
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-06-08 16:56:52 +0800

    train yolof18_exp

--- train.sh
+++ train.sh
@@ -2,10 +2,10 @@ python train.py \
         --cuda \
         -d coco \
         --root /mnt/share/ssd2/dataset/ \
-        -v fcos-rt-ota \
-        -lr 0.01 \
+        -v yolof18_exp \
+        -lr 0.03 \
         -lr_bk 0.01 \
         --batch_size 16 \
         --lr_scheduler step \
-        --schedule 3x \
+        --schedule 1x \
         --grad_clip_norm 4.0
