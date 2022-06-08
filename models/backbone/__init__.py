52a9753ef5bd620094086f0305a9e13f787cb472
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-06-08 21:04:45 +0800

    train yolof18_exp

--- models/backbone/__init__.py
+++ models/backbone/__init__.py
@@ -1,6 +1,4 @@
 from .resnet import build_resnet
-from .convnext import build_convnext
-from .vggnet import vgg16
 from .darknet53 import darknet53
 
 
@@ -16,21 +14,6 @@ def build_backbone(model_name='resnet50-d',
         model, feat_dim = build_resnet(model_name=model_name, 
                                        pretrained=pretrained,
                                        norm_type=norm_type)
-
-    elif 'convnext' in model_name:
-        if model_name[-1] == 'd':
-            model, feat_dim = build_convnext(model_name=model_name,
-                                            pretrained=pretrained,
-                                            res_dilation=True,
-                                            in_22k=in_22k)
-        else:
-            model, feat_dim = build_convnext(model_name=model_name,
-                                            pretrained=pretrained,
-                                            res_dilation=False,
-                                            in_22k=in_22k)
-    
-    elif model_name == 'vgg16':
-        model, feat_dim = vgg16(pretrained=pretrained)
 
     elif model_name == 'darknet53':
         model, feat_dim = darknet53(pretrained=pretrained, 
