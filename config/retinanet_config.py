56a9d326d9f2abcc91f643d56b03216e49feef36
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-06-17 09:19:01 +0800

    debug

--- config/retinanet_config.py
+++ config/retinanet_config.py
@@ -4,10 +4,10 @@
 retinanet_config = {
     'retinanet18': {
         # input
-        'train_min_size': 800,
-        'train_max_size': 1333,
-        'test_min_size': 800,
-        'test_max_size': 1333,
+        'train_min_size': 512,
+        'train_max_size': 512,
+        'test_min_size': 512,
+        'test_max_size': 512,
         'format': 'RGB',
         'pixel_mean': [0.485, 0.456, 0.406],
         'pixel_std': [0.229, 0.224, 0.225],
